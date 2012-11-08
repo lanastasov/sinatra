53a3cb869b108297f8ffcb5a5b0e1001b198b3da
unknown <lyubomir.anastasov@ecommera-103.ecommera.co.uk>
2012-11-08 16:02:05 +0200

    third commit

--- applications/gallery/app.rb
+++ applications/gallery/app.rb
@@ -1,13 +1,14 @@
 require 'sinatra'
 require 'sinatra/reloader'
-
+=begin
+=end
 helpers do
 	def get_files(path)
-			dir_list_array = Array.new
-			Find.find(path) do |f|
-				dir_list_array << File.basename(f, ".*") if !File.directory?(f)
-			end
-			return dir_list_array
+		dir_list_array = Array.new
+		Find.find(path) do |f|
+			dir_list_array << File.basename(f, ".*") if !File.directory?(f)
+		end
+		return dir_list_array
 	end
 
 end
